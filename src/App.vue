062075674e084133c9859f9f675562e3737b9aad
FranzBurneo <franzdavidburneo@gmail.com>
2023-07-30 14:47:26 -0500

    feat: First test with vue components showing posts from api

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import BlogPost from './components/BlogPost.vue';
+  import {ref} from "vue";
+
+  const posts = ref([]);
+  const favorito = ref('');
+
+  const cambiarFavorito = (title) => {
+    favorito.value = title;
+  }
+
+  fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => posts.value = json)
+
+</script>
+<template>
+  <div class="container">
+    <h1>Posts</h1>
+    <h2>My favorite posts: {{favorito}}</h2>
+    <BlogPost 
+      v-for="post in posts"
+      :key = "post.id"
+      :id="post.id"
+      :title="post.title"
+      :body="post.body"
+      :cambiarFavorito="cambiarFavorito"
+    />
+  </div>
+</template>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,34 @@
+<script setup>
+    // defineProps(["id", "title", "body"])
+    defineProps({
+        title: String,
+        id: Number,
+        body: {
+            type: String,
+            default: "No description"
+        },
+        cambiarFavorito: Function,
+    });
+
+    // const emit = defineEmits(['cambiarFavoritoNombre'])
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title"
+            >
+                {{id}} - {{title}}
+            </h5>
+            <p>
+                {{ body }}
+            </p>
+            <button 
+            @click="cambiarFavorito(title)"
+            class="btn btn-outline-primary"
+            >
+                Favorite
+            </button>
+        </div>
+    </div>
+</template>
