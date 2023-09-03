b2ddf5e6736791ab27dcdfd68385b6d9bef34c6c
FranzBurneo <franzdavidburneo@gmail.com>
2023-09-03 11:07:50 -0500

    feat sincro files

---
+++ src/components/PaginatePost.vue
@@ -0,0 +1,26 @@
+<script setup>
+    defineProps(['inicio', 'fin', 'maxLength']);
+    const emit = defineEmits(['next', 'previus']);
+
+    const activarSiguiente = () => {
+        console.log("me diste siguiente");
+        emit("next");
+    }
+</script>
+
+<template>
+    <div class="btn-group" role="group" aria-label="Basic example">
+    <button type="button" class="btn btn-outline-primary"
+    @click="emit('previus')"
+    :disabled="inicio<=0"
+    >
+    Previus
+    </button>    
+    <button type="button" class="btn btn-outline-primary"
+    @click="activarSiguiente"
+    :disabled="fin>=maxLength"
+    >
+    Next
+    </button>
+    </div>
+</template>
